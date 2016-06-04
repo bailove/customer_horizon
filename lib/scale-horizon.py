90ad0588aa492aa1b37e8ac8a3d79f7cee008220
bailove <bailove@gmail.com>
2016-06-05 07:43:07 +0800

    change customer

--- lib/scale-horizon.py
+++
@@ -1,7 +0,0 @@
-import horizon
-
-# remove the 'change password panel'
-settings_dashboard = horizon.get_dashboard("settings")
-password_panel = settings_dashboard.get_panel("password")
-settings_dashboard.unregister(password_panel.__class__)
-
